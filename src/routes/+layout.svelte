8db7c2a23bb25b826c6ccb5376b59062cfd06251
board <boardergamezdiscord@gmail.com>
2026-05-06 21:35:26 +0200

    Started on my amazing web dev journey

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.ico';
+	import hackclub from '$lib/assets/hackclub.png';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+<nav>
+	<img src={hackclub} alt='hackclub.com'>
+</nav>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="text-scale" content="scale" />
+	</head>
+	<body>
+		<h1>Frog</h1>
+		<h2>YS:Cool Projects
+			WS: A cardboard cutout or bodypillow of @frog
+		</h2>
+	</body>
+</html>
